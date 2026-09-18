3476289e2701f700529c75e7f3aa43cc7a42b6cd
Ashritha gn <ashrithagn01@gmail.com>
2026-09-19 03:24:52 +0530

    Update Vite config to remove Tailwind CSS
    Removed Tailwind CSS plugin from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { base: '/Ashritha-Portfolio/', } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/Ashritha-Portfolio/',
+  plugins: [react()],
 })
